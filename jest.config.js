19eaf3226e5e8160f05c0aa8a0cb538a930e2a87
Lorenzo Franceschini <lordkenzo@outlook.com>
2020-11-26 22:21:54 +0100

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+mpdule.exports = {
+  roots: ['<rootDir>/src'],
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1'
+  }
+}
